e3ba926b50bd8b1550272befb410750189fc7a22
Surface <da.ga.mo.94@gmail.com>
2019-06-12 17:56:51 -0600

    GIFs display to page. Only need to display them paused and play when they are clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 var gifsArr = ["zebra", "linx", "tiger", "elephant"];
+var displayLimit = 10;
 
 
 function displayGifs(){
     var currentGif = $(this).attr("data-name")
     alert(currentGif);
-    var displayLimit = 10;
 
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
     currentGif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=" + displayLimit;
@@ -14,6 +14,22 @@ function displayGifs(){
         method: "GET"
     }).then(function(response){
         console.log(response);
+        var results = response.data;
+
+        for(var i = 0; i < results.length; i++){
+            if(results[i].rating != 'r' && results[i] != 'pg-13'){
+                var gifDiv = $("<div>").addClass("float-left");
+                var ratingP = $("<p>");
+                var gifImage = $("<img>");
+
+                ratingP.append("Rating: " + results[i].rating);
+                gifImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.append(ratingP, gifImage);
+
+                $("#gifs-view").prepend(gifDiv);
+            }
+        }
     });
 }
 
